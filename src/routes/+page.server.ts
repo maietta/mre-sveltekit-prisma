d9455fa06d1c0a05afb29ea7624102dfe167d6ca
Nick Maietta <nick@premoweb.com>
2024-06-30 07:29:51 -0700

    Fix typos, get page started.

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,11 @@
+import type { PageServerLoad } from './$types';
+import { prisma } from '$lib/server/prisma'; // Single source of truth for this specific db connection.
+
+export const load = (async () => {
+
+    const data = await prisma.user.findMany();
+
+    return {
+        users: data
+    };
+}) satisfies PageServerLoad;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<hr />
+
+This is a minimum reproducable example for wiring up Prisma Client with SvelteKit and within the Bun runtime enviornment.
